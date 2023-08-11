4208362ea66a251e4d79004a85f00e119a3b7169
Juan <saraviamartin16@hotmail.com>
2023-08-11 11:59:34 -0300

    creando contexto para el carrito

---
+++ src/context/cart.jsx
@@ -0,0 +1,46 @@
+
+import { createContext, useState } from "react";
+
+//Crear contexto
+export const CartContext = createContext()
+
+//Crear Provider
+export function CartProvider ({children }) {
+    const [cart, setCart]=useState([])
+
+    const addToCart = product => {
+        const productInCartIndex = cart.findIndex(item => item.id === product.id)
+
+        if(productInCartIndex >= 0){
+            //Una forma seria usando structuredClone
+            const newCart = structuredClone(cart)
+            newCart[productInCartIndex] += 1
+            return setCart[newCart]
+        }
+
+        //Si el producto no esta en el carrito
+        setCart(prevState => ([
+            ...prevState,
+            {
+                ...product,
+                quantity: 1
+            }
+        ]))
+    }
+
+
+    const clearCart = () => {
+        setCart([])
+    }
+
+    return(
+        <CartContext.Provider value={{
+            cart,
+            addToCart,
+            clearCart,
+        }}
+        >
+            {children}
+        </CartContext.Provider>
+    )
+}
